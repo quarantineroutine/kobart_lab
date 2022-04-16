c60ad5a2a734444cc94487972071e0cce00483ce
quarantineroutine <103600062+quarantineroutine@users.noreply.github.com>
2022-04-16 09:37:42 +0000

    fix errors

--- distilkobart.py
+++ distilkobart.py
@@ -1,9 +1,12 @@
+import os
 import warnings
+from typing import List, Union, Optional, Callable
 
-from typing import List
+import torch
 from torch import nn
 
 from transformers import BartForConditionalGeneration
+from transformers.modeling_utils import save_pretrained
 
 
 LAYERS_TO_COPY = {
@@ -20,63 +23,68 @@ LAYERS_TO_COPY = {
 
 
 class DistilKoBart(nn.Module):
-	def __init__(self, teacher, e=None, d=None):
-		super(DistilKoBart, self).__init__()
+    def __init__(self, teacher, e=None, d=None):
+        super(DistilKoBart, self).__init__()
 
-		self.teacher = teacher
-		init_kwargs = teacher.config.to_diff_dict()
+        self.teacher = teacher
+        init_kwargs = teacher.config.to_diff_dict()
 
-		teacher_e, teacher_d = teacher.config.encoder_layers, teacher.config.decoder_layers
-		if e is None:
-			e = teacher_e
-		if d is None:
-			d = teacher_d
-		init_kwargs.update({"encoder_layers": e, "decoder_layers": d})
+        teacher_e, teacher_d = teacher.config.encoder_layers, teacher.config.decoder_layers
+        if e is None:
+            e = teacher_e
+        if d is None:
+            d = teacher_d
+        init_kwargs.update({"encoder_layers": e, "decoder_layers": d})
 
-		# Copy weights
-		student_cfg = teacher.config_class(**init_kwargs)
-		self.student = BartForConditionalGeneration(cfg=student_cfg)
-		self.student.load_state_dict(self.teacher.state_dict(), strict=False)
+        # Copy weights
+        student_cfg = teacher.config_class(**init_kwargs)
+        self.config = student_cfg
+        self.model = BartForConditionalGeneration(config=student_cfg)
+        self.model.load_state_dict(self.teacher.state_dict(), strict=False)
 
-		e_layers_to_copy: List[int] = self.pick_layers_to_copy(e, teacher_e)
-		d_layers_to_copy: List[int] = self.pick_layers_to_copy(d, teacher_d)
+        e_layers_to_copy: List[int] = self.pick_layers_to_copy(e, teacher_e)
+        d_layers_to_copy: List[int] = self.pick_layers_to_copy(d, teacher_d)
 
-		self.copy_layers(
-			self.teacher.model.encoder.layers,
-			self.student.model.encoder.layers,
-			e_layers_to_copy
-		)
-		self.copy_layers(
-			self.teacher.model.decoder.layers,
-			self.student.model.decoder.layers,
-			d_layers_to_copy
-		)
-		self.teacher = None
-
-
-	def forward(self, input_ids, attention_mask, labels, decoder_input_ids=None, decoder_attention_mask=None):
-		return self.student(
-			input_ids=input_ids,
-			attention_mask=attention_mask,
-			labels=labels,
-			decoder_input_ids=decoder_input_ids,
-			decoder_attention_mask=decoder_attention_mask,
-		)
+        self.copy_layers(
+            self.teacher.model.encoder.layers,
+            self.model.model.encoder.layers,
+            e_layers_to_copy
+        )
+        self.copy_layers(
+            self.teacher.model.decoder.layers,
+            self.model.model.decoder.layers,
+            d_layers_to_copy
+        )
+        self.teacher = None
 
 
-	def copy_layers(self, src_layers: nn.ModuleList, dest_layers: nn.ModuleList, layers_to_copy: List[int]) -> None:
-		layers_to_copy = nn.ModuleList([src_layers[i] for i in layers_to_copy])
-		assert len(dest_layers) == len(layers_to_copy), f"{len(dest_layers)} != {len(layers_to_copy)}"
-		dest_layers.load_state_dict(layers_to_copy.state_dict())
+    def forward(self, input_ids, attention_mask, decoder_input_ids=None, decoder_attention_mask=None, **kwargs):
+        return self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            decoder_input_ids=decoder_input_ids,
+            decoder_attention_mask=decoder_attention_mask,
+            **kwargs
+        )
 
 
-	def pick_layers_to_copy(self, n_student, n_teacher):
-		try:
-			val = LAYERS_TO_COPY[n_teacher][n_student]
-			return val
-		except KeyError:
-			if n_student != n_teacher:
-				warnings.warn(
-					f"no hardcoded layers to copy for teacher {n_teacher} -> student {n_student}, defaulting to first {n_student}"
-				)
-			return list(range(n_student))
+    def copy_layers(self, src_layers: nn.ModuleList, dest_layers: nn.ModuleList, layers_to_copy: List[int]) -> None:
+        layers_to_copy = nn.ModuleList([src_layers[i] for i in layers_to_copy])
+        assert len(dest_layers) == len(layers_to_copy), f"{len(dest_layers)} != {len(layers_to_copy)}"
+        dest_layers.load_state_dict(layers_to_copy.state_dict())
+
+
+    def pick_layers_to_copy(self, n_student, n_teacher):
+        try:
+            val = LAYERS_TO_COPY[n_teacher][n_student]
+            return val
+        except KeyError:
+            if n_student != n_teacher:
+                warnings.warn(
+                    f"no hardcoded layers to copy for teacher {n_teacher} -> student {n_student}, defaulting to first {n_student}"
+                )
+            return list(range(n_student))
+    
+
+    def save_pretrained(self, *args, **kwargs):
+        self.model.save_pretrained(*args, **kwargs)
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from glob import glob
 
@@ -16,16 +17,18 @@ from lightning_modules import DefaultModule, RDropModule, R3FModule
 
 parser = argparse.ArgumentParser(description="Fine-tuning Korean Dialogue Summarization with KoBART")
 
-parser.add_argument("--accumulate-grad-batches", type=int, default=1, help="the number of gradident accumulation steps")
+parser.add_argument("--accumulate-grad-batches", type=int, default=2, help="the number of gradident accumulation steps")
 parser.add_argument("--batch-size", type=int, default=16, help="training batch size")
 parser.add_argument("--dialogue-max-seq-len", type=int, default=256, help="dialogue max sequence length")
 parser.add_argument("--epochs", type=int, default=5, help="the number of training epochs")
 parser.add_argument("--evaluate-interval", type=int, default=500, help="validation interval")
-parser.add_argument("--gpus", type=int, default=2, help="the number of gpus")
+parser.add_argument("--gpus", type=int, default=2, help="the number of gpus(set to 0 if using cpu)")
 parser.add_argument("--logging-interval", type=int, default=100, help="logging interval")
 parser.add_argument("--max-learning-rate", type=float, default=2e-4, help="max learning rate")
 parser.add_argument("--method", type=str, choices=["default", "rdrop", "r3f"], default="default", help="training method")
 parser.add_argument("--min-learning-rate", type=float, default=1e-5, help="min Learning rate")
+parser.add_argument("--num-encoder-layer", type=int, default=6, help="the number of encoder layer")
+parser.add_argument("--num-decoder-layer", type=int, default=3, help="the number of decoder layer")
 parser.add_argument("--output-dir", type=str, required=True, help="output directory path to save artifacts")
 parser.add_argument("--rdrop-alpha", type=float, default=0.7, help="rdrop alpha parameter (only used with `rdrop` method)")
 parser.add_argument("--r3f-lambda", type=float, default=1.0, help="r3f lambda parameter (only used with `r3f` method)")
@@ -39,10 +42,13 @@ parser.add_argument("--warmup-rate", type=float, default=0.05, help="warmup step
 
 
 def main(args: argparse.Namespace) -> None:
-    seed_everything(args.seed)
+    os.environ["TOKENIZERS_PARALLELISM"] = "true"
+    seed_everything(args.seed, workers=True)
 
     tokenizer = PreTrainedTokenizerFast.from_pretrained('hyunwoongko/kobart', sep_token='<unused0>', cls_token='<unused1>')
-    model = DistilKoBart(BartForConditionalGeneration.from_pretrained('hyunwoongko/kobart'))
+    teacher = BartForConditionalGeneration.from_pretrained('hyunwoongko/kobart')
+    student = DistilKoBart(teacher, e=args.num_encoder_layer, d=args.num_decoder_layer)
+    print(f'use DistilKoBart-{args.num_encoder_layer}-{args.num_decoder_layer}')
 
     train_dataset = DialogueSummarizationDataset(
         paths=glob(args.train_dataset_pattern),
@@ -60,14 +66,14 @@ def main(args: argparse.Namespace) -> None:
         use_summary=True,
     )
 
-    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=args.val_batch_size)
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size, num_workers=4)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=args.val_batch_size, num_workers=4)
 
     total_steps = len(train_dataloader) * args.epochs
 
     if args.method == "rdrop":
         lightning_module = RDropModule(
-            model,
+            student.model,
             total_steps,
             args.max_learning_rate,
             args.min_learning_rate,
@@ -77,7 +83,7 @@ def main(args: argparse.Namespace) -> None:
         )
     elif args.method == "r3f":
         lightning_module = R3FModule(
-            model,
+            student.model,
             total_steps,
             args.max_learning_rate,
             args.min_learning_rate,
@@ -87,13 +93,14 @@ def main(args: argparse.Namespace) -> None:
         )
     else:
         lightning_module = DefaultModule(
-            model,
+            student.model,
             total_steps,
             args.max_learning_rate,
             args.min_learning_rate,
             args.warmup_rate,
             args.output_dir + '/models/' + args.log_run_name,
         )
+    print(f'use method {args.method}')
 
     train_loggers = [
         TensorBoardLogger(
@@ -111,21 +118,37 @@ def main(args: argparse.Namespace) -> None:
         mode='min',
     )
 
-    trainer = Trainer(
-        logger=train_loggers,
-        max_epochs=args.epochs,
-        log_every_n_steps=args.logging_interval,
-        val_check_interval=args.evaluate_interval,
-        accumulate_grad_batches=args.accumulate_grad_batches,
-        callbacks=[
-            LearningRateMonitor(logging_interval="step"),
-            EarlyStopping(monitor="val_loss", patience=5, mode="min"),
-            checkpoint_callback
-        ],
-        gpus=args.gpus,
-        strategy='ddp',
-    )
+    if args.gpus == 0:
+        trainer = Trainer(
+            logger=train_loggers,
+            max_epochs=args.epochs,
+            log_every_n_steps=args.logging_interval,
+            val_check_interval=args.evaluate_interval,
+            accumulate_grad_batches=args.accumulate_grad_batches,
+            callbacks=[
+                LearningRateMonitor(logging_interval="step"),
+                EarlyStopping(monitor="val_loss", patience=3, mode="min"),
+                checkpoint_callback
+            ],
+            accelerator="cpu",
+        )
+    else:
+        trainer = Trainer(
+            logger=train_loggers,
+            max_epochs=args.epochs,
+            log_every_n_steps=args.logging_interval,
+            val_check_interval=args.evaluate_interval,
+            accumulate_grad_batches=args.accumulate_grad_batches,
+            callbacks=[
+                LearningRateMonitor(logging_interval="step"),
+                EarlyStopping(monitor="val_loss", patience=3, mode="min"),
+                checkpoint_callback
+            ],
+            gpus=args.gpus,
+            strategy='ddp',
+        )
 
+    print('start to train...')
     trainer.fit(lightning_module, train_dataloader, valid_dataloader)
 
 
